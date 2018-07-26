1fc1bcc2070cf0f22dbfe7432d91a51f1bea433f
jeSoosRemirez <dkhomych0091@gmail.com>
2018-07-26 22:52:34 +0300

    Change db engine to postgresql

--- four_words/four_words/settings.py
+++ four_words/four_words/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # My apps
+    'learn_words'
 ]
 
 MIDDLEWARE = [
@@ -75,8 +78,12 @@ WSGI_APPLICATION = 'four_words.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'four_words',
+        'USER': 'dmytro',
+        'PASSWORD': '1111',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
