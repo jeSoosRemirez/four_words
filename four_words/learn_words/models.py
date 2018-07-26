1d5ff9814bf47a62e462233716c5da105c503085
jeSoosRemirez <dkhomych0091@gmail.com>
2018-07-26 22:57:03 +0300

    Create model FourWords with main fields and __str__ method

--- four_words/learn_words/models.py
+++ four_words/learn_words/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class FourWords(models.Model):
+
+    noun = models.CharField(max_length=50, blank=True)
+    adjective = models.CharField(max_length=50, blank=True)
+    adverb = models.CharField(max_length=50, blank=True)
+    verb = models.CharField(max_length=50, blank=True)
+    pub_date = models.DateTimeField('date published')
+
+    def __str__(self):
+        return f'{self.noun} | {self.adjective} | {self.adverb} | {self.verb}'
+
+
