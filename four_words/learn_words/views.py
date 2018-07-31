754e38865e5f41de372c371b4938b0be67f68bd1
jeSoosRemirez <dkhomych0091@gmail.com>
2018-07-31 15:40:08 +0300

    Create two views

--- four_words/learn_words/views.py
+++ four_words/learn_words/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+from django.shortcuts import render, get_object_or_404
+
+from .models import FourWords
+
+
+def four_words_page(request):
+    four_words = FourWords.objects.all()
+    return render(request, 'four_words.html', {'four_words': four_words})
+
+
+def word_page(request, word_id):
+    word = get_object_or_404(FourWords, id=word_id)
+    return render(request, 'word_info.html', {'word': word})
