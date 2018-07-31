09b0dae7437d00234cb4ac09a6f77d6289339588
jeSoosRemirez <dkhomych0091@gmail.com>
2018-07-31 15:34:29 +0300

    Add static folder to settings

--- four_words/four_words/settings.py
+++ four_words/four_words/settings.py
@@ -124,4 +124,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
+
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'dev_static'),
+)
