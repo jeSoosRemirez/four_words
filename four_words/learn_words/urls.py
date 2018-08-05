48cb415e465a9190639389dd09872d72bf21713c
jeSoosRemirez <dkhomych0091@gmail.com>
2018-08-05 17:03:43 +0300

    Add url word-info and user-words

--- four_words/learn_words/urls.py
+++ four_words/learn_words/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
-from .views import four_words_page, word_page
+from .views import four_words_page, word_page,user_words
 
 urlpatterns = [
+    # path('add-new-word/', AddNewWord, name='add_new_word'),
     path('four-words/', four_words_page, name='four-words'),
-    path('<int:word_id>/', word_page, name='word-info')
+    path('word/<int:word_id>/', word_page, name='word-info'),
+    path('user/<int:user_id>/', user_words, name='user-words')
 ]
