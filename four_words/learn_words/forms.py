e575852da0d5e81d380b26b32501d3fd03bd198d
jeSoosRemirez <dkhomych0091@gmail.com>
2018-08-07 11:22:13 +0300

    Create form EditNewWord

--- four_words/learn_words/forms.py
+++ four_words/learn_words/forms.py
@@ -2,7 +2,25 @@ from django import forms
 
 from .models import FourWords
 
+WORD_CHOICES = (
+    (1, 'noun'),
+    (2, 'adjective'),
+    (3, 'adverb'),
+    (4, 'verb'),
+)
+
 
 class AddNewWord(forms.Form):
     word = forms.CharField(label='Word', max_length=100)
+    word_type = forms.ChoiceField(choices=WORD_CHOICES)
 
+
+class EditFourWords(forms.ModelForm):
+
+    class Meta:
+        model = FourWords
+        fields = ('noun', 'adjective', 'adverb', 'verb',
+                  'noun_description', 'adjective_description', 'adverb_description', 'verb_description',
+                  'noun_example', 'adjective_example', 'adverb_example', 'verb_example',
+                  'noun_translate', 'adjective_translate', 'adverb_translate', 'verb_translate'
+                  )
