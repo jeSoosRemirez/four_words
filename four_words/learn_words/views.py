64ac838d6df4c58445de923b2a26247581821755
jeSoosRemirez <dkhomych0091@gmail.com>
2018-08-07 11:24:17 +0300

    Add def new_word

--- four_words/learn_words/views.py
+++ four_words/learn_words/views.py
@@ -37,3 +37,63 @@ def user_words(request, user_id):
     user = get_object_or_404(User, id=user_id)
     words = FourWords.objects.filter(author=user)
     return render(request, 'user-words.html', {'words': words})
+
+def new_word(request):
+    if request.method == "POST":
+        form = AddNewWord(request.POST)
+        if form.is_valid():
+            word = form.cleaned_data['word']
+            word_type = form.cleaned_data['word_type']
+
+            author_id = request.user.id
+            pub_date = timezone.now()
+            if word_type == '1':
+                four_words = FourWords(
+                    author_id=author_id,
+                    pub_date=pub_date,
+                    noun=word,
+                )
+                four_words.save()
+                return redirect('word-info', word_id=four_words.pk)
+            elif word_type == '2':
+                four_words = FourWords(
+                    author_id=author_id,
+                    pub_date=pub_date,
+                    adjective=word,
+                )
+                four_words.save()
+                return redirect('word-info', word_id=four_words.pk)
+            elif word_type == '3':
+                four_words = FourWords(
+                    author_id=author_id,
+                    pub_date=pub_date,
+                    adverb=word,
+                )
+                four_words.save()
+                return redirect('word-info', word_id=four_words.pk)
+            elif word_type == '4':
+                four_words = FourWords(
+                    author_id=author_id,
+                    pub_date=pub_date,
+                    verb=word,
+                )
+                four_words.save()
+                return redirect('word-info', word_id=four_words.pk)
+    else:
+        form = AddNewWord()
+    return render(request, 'edit_word.html', {'form': form})
+
+def edit_four_words(request, word_id):
+    four_words = get_object_or_404(FourWords, pk=word_id)
+    if request.method == "POST":
+        form = EditFourWords(request.POST, instance=four_words)
+        if form.is_valid():
+            four_words = form.save(commit=False)
+            four_words.author_id = request.user
+            four_words.pub_date = timezone.now()
+            four_words.save()
+            return redirect('word-info', word_id=four_words.pk)
+    else:
+        form = EditFourWords(instance=four_words)
+    return render(request, 'edit_four_words.html', {'form': form})
+
