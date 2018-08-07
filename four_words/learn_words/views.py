306586bba1126eb0991792c1403d2acf199cb503
jeSoosRemirez <dkhomych0091@gmail.com>
2018-08-07 08:27:07 +0300

    Little fixes

--- four_words/learn_words/views.py
+++ four_words/learn_words/views.py
@@ -3,27 +3,15 @@ from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404, redirect
 
 from .models import FourWords
-# from .forms import AddNewWord
 
 from django.views.generic import View
 
 from django.contrib.auth.models import User
 
-def four_words_page(request):
-    search = request.GET.get('search', '')
-    four_words = FourWords.objects.filter(noun__contains=search)
-    return render(request, 'four_words.html', {'four_words': four_words})
-
-
-def word_page(request, word_id):
-    word = get_object_or_404(FourWords, id=word_id)
-    return render(request, 'word_info.html', {'word': word})
-
-
 class AddWord(View):
 
     def get(self, request):
-        return render(request, 'add_word.html', {'form': AddNewWord})
+        return render(request, 'base.html', {'form': AddNewWord})
 
     def post(self, request):
         form = AddNewWord(request.POST)
