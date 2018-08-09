e7906a5ba2814e16fb48cf2f6e477bc7b6d30259
jeSoosRemirez <dkhomych0091@gmail.com>
2018-08-09 12:34:20 +0300

    Add url delete-word

--- four_words/learn_words/urls.py
+++ four_words/learn_words/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import four_words_page, word_page, user_words, new_word, edit_four_words
+from .views import four_words_page, word_page, user_words, new_word, edit_four_words, delete_word
 
 urlpatterns = [
     # path('add-new-word/', AddNewWord, name='add_new_word'),
@@ -8,5 +8,6 @@ urlpatterns = [
     path('word/<int:word_id>/', word_page, name='word-info'),
     path('user/<int:user_id>/', user_words, name='user-words'),
     path('four-words/<int:word_id>/edit/', edit_four_words, name='four-words-edit'),
+    path('four-words/<int:word_id>/delete/', delete_word, name='four-words-delete'),
     path('four-words/new/', new_word, name='new-word'),
 ]
