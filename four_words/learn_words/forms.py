7cb2b888ca881e3f0651d9e5c3c17497f6e24f5a
jeSoosRemirez <dkhomych0091@gmail.com>
2018-08-09 11:59:03 +0300

    Add other to class EditFourWords

--- four_words/learn_words/forms.py
+++ four_words/learn_words/forms.py
@@ -19,8 +19,9 @@ class EditFourWords(forms.ModelForm):
 
     class Meta:
         model = FourWords
-        fields = ('noun', 'adjective', 'adverb', 'verb',
+        fields = ('noun', 'adjective', 'adverb', 'verb', 'other',
                   'noun_description', 'adjective_description', 'adverb_description', 'verb_description',
-                  'noun_example', 'adjective_example', 'adverb_example', 'verb_example',
-                  'noun_translate', 'adjective_translate', 'adverb_translate', 'verb_translate'
+                  'other_description',
+                  'noun_example', 'adjective_example', 'adverb_example', 'verb_example', 'other_example',
+                  'noun_translate', 'adjective_translate', 'adverb_translate', 'verb_translate', 'other_translate'
                   )
