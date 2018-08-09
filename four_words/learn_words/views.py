97a5d98f7f2fc48eed8dacf4e5a412ba4dc528d1
jeSoosRemirez <dkhomych0091@gmail.com>
2018-08-09 09:51:42 +0300

    Add fixes for many problems

--- four_words/learn_words/views.py
+++ four_words/learn_words/views.py
@@ -46,42 +46,78 @@ def new_word(request):
         form = AddNewWord(request.POST)
         if form.is_valid():
             word = form.cleaned_data['word']
-            word_type = form.cleaned_data['word_type']
 
             author_id = request.user.id
             pub_date = timezone.now()
-            if word_type == '1':
+            url = 'https://od-api.oxforddictionaries.com:443/api/v1/entries/en/{}'.format(word.lower())
+            r = requests.get(url, headers={'app_id': settings.OXFORD_APP_ID, 'app_key': settings.OXFORD_APP_KEY})
+            if r.status_code == 200:
+                response = r.json()
+                result = response['results'][0]['lexicalEntries'][0]
+                word_type = result['lexicalCategory']
+                description = result['entries'][0]['senses'][0].get('definitions') if not None else ''
+                word_description = description[0] if description else ''
+                examples = result['entries'][0]['senses'][0].get('examples') if not None else ''
+                word_example = examples[0]['text'] if examples else ''
+                if word_type == 'Noun':
+                    four_words = FourWords(
+                        author_id=author_id,
+                        pub_date=pub_date,
+                        noun=word,
+                        noun_description=word_description,
+                        noun_example=word_example,
+                    )
+                    four_words.save()
+                    return redirect('word-info', word_id=four_words.pk)
+                elif word_type == 'Adjective':
+                    four_words = FourWords(
+                        author_id=author_id,
+                        pub_date=pub_date,
+                        adjective=word,
+                        adjective_description=word_description,
+                        adjective_example=word_example,
+                    )
+                    four_words.save()
+                    return redirect('word-info', word_id=four_words.pk)
+                elif word_type == 'Adverb':
+                    four_words = FourWords(
+                        author_id=author_id,
+                        pub_date=pub_date,
+                        adverb=word,
+                        adverb_description=word_description,
+                        adverb_example=word_example,
+                    )
+                    four_words.save()
+                    return redirect('word-info', word_id=four_words.pk)
+                elif word_type == 'Verb':
+                    four_words = FourWords(
+                        author_id=author_id,
+                        pub_date=pub_date,
+                        verb=word,
+                        verb_description=word_description,
+                        verb_example=word_example,
+                    )
+                    four_words.save()
+                    return redirect('word-info', word_id=four_words.pk)
+                else:
+                    four_words = FourWords(
+                        author_id=author_id,
+                        pub_date=pub_date,
+                        other=word,
+                        other_description=word_description,
+                        other_example=word_example,
+                    )
+                    four_words.save()
+                    return redirect('word-info', word_id=four_words.pk)
+            else:
                 four_words = FourWords(
                     author_id=author_id,
                     pub_date=pub_date,
-                    noun=word,
+                    other=word,
                 )
                 four_words.save()
                 return redirect('word-info', word_id=four_words.pk)
-            elif word_type == '2':
-                four_words = FourWords(
-                    author_id=author_id,
-                    pub_date=pub_date,
-                    adjective=word,
-                )
-                four_words.save()
-                return redirect('word-info', word_id=four_words.pk)
-            elif word_type == '3':
-                four_words = FourWords(
-                    author_id=author_id,
-                    pub_date=pub_date,
-                    adverb=word,
-                )
-                four_words.save()
-                return redirect('word-info', word_id=four_words.pk)
-            elif word_type == '4':
-                four_words = FourWords(
-                    author_id=author_id,
-                    pub_date=pub_date,
-                    verb=word,
-                )
-                four_words.save()
-                return redirect('word-info', word_id=four_words.pk)
+
     else:
         form = AddNewWord()
     return render(request, 'edit_word.html', {'form': form})
